0e5d1b7fbd763ae90841cc1ace0018b7e781ec27
Gonzalo Benoffi <benoffi11@gmail.com>
2017-04-26 14:29:14 -0300

    Ejercicio7

---
+++ Ejercicio7/src/Metodos.java
@@ -0,0 +1,10 @@
+public class Metodos {
+ 
+    public static final String nombres[] = {"Fernando", "Laura", "Pepe", "Eufrasio"};
+ 
+    public static int generaNumeroEnteroAleatorio(int minimo, int maximo) {
+        int num = (int) (Math.random() * (minimo - (maximo + 1)) + (maximo + 1));
+        return num;
+    }
+ 
+}
