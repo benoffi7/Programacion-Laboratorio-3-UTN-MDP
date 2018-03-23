5ef6dbb433fbc9df1bee3cf18113d5366fd2a60d
Gonzalo Benoffi <benoffi11@gmail.com>
2018-03-23 09:20:09 -0300

    Maquina expendedora

---
+++ 2018/MaquinaDeBoletos/src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import clases.MaquinaExpendedora;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+	}
+
+}
---
+++ 2018/MaquinaDeBoletos/src/clases/MaquinaExpendedora.java
@@ -0,0 +1,70 @@
+package clases;
+
+public class MaquinaExpendedora {
+
+	private int precio;
+	private int saldo;
+	private int total;
+	
+	public MaquinaExpendedora(int precioDeAfuera)
+	{
+		precio = precioDeAfuera;
+		saldo = 0;
+		total = 0;
+	}
+	
+	//metodos de acceso
+	
+	public int obtenerPrecio() // getPrecio
+	{
+		return precio;
+	}
+	
+	public int obtenerSaldo() // getSaldo
+	{
+		return saldo;
+	}
+	
+	// metodo de modificacion
+	
+	public void imprmirBoleto()
+	{
+		total = total + saldo;
+		saldo = 0 ;
+		System.out.println("boleto impreso!");
+	}
+	
+	public void ingresarDinero(int dinero)
+	{
+		saldo = saldo + dinero;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof MaquinaExpendedora)
+		{
+			MaquinaExpendedora otraMaquina = (MaquinaExpendedora)obj;
+			if (precio == otraMaquina.obtenerPrecio()) return true;
+			else return false;	
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Mi precio es de "+precio+" y mi saldo es de "+saldo+" y mi recaudacion es de "+total;
+	}
+	
+	public String devolverNombreClase()
+	{
+		return getClass().getName();
+	}
+	
+	
+}
