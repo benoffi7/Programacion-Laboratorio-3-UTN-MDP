fa863538faf33ec2b7b2df58fe0a1091e3bd1e46
Gonzalo Benoffi <benoffi11@gmail.com>
2019-03-22 11:08:13 -0300

    Ejemplos java hola mundo

---
+++ 2019/hola/src/paquete/Main.java
@@ -0,0 +1,20 @@
+package paquete;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hola clase");
+		int numero = 6;
+		sumar(numero);
+	}
+	
+	public static void sumar(int numero)
+	{
+		int numero1 = 1;
+		int suma = numero+numero1;
+		System.out.println(suma);
+
+	}
+
+}
