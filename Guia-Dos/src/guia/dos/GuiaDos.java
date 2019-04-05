d9d5f757c85f222e3bcb5725ce8360325c1f030f
Matias Nicoletti <matiasnicolettimdp@gmail.com>
2019-04-05 09:12:29 -0300

    Guia Dos

---
+++ Guia-Dos/src/guia/dos/GuiaDos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia.dos;
+
+import guia.dos.pantalla.CrearCuenta;
+import guia.dos.pantalla.Operacion;
+
+
+
+public class GuiaDos {
+
+    public static void main(String[] args) {
+
+        CrearCuenta cuenta = new CrearCuenta();
+        cuenta.setVisible(true);
+        
+        
+        
+    }
+
+}
